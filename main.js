1b48ef3b49e92b7a736ed370926027779d885b71
Chelosgd <salgado.marcelo@outlook.com>
2023-12-03 20:15:16 -0600

    cors3

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ const mongoURL = process.env.MONGO_URL;
 const app = express();
 
 app.use('', routes);
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    next();
+})
 
 app.get('', (req, res) => {
     res.send('API works');
