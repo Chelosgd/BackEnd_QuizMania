0fee1862b0f83a95a4f0e4715febc46f22881ccd
Chelosgd <salgado.marcelo@outlook.com>
2023-12-04 01:02:04 -0600

    Upload image

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const path = require('path');
 const routes = require("./routes/index");
 const cors = require('cors');
+const multer = require('multer');
 
 const port = process.env.PORT || 3000; 
 const secretKey = process.env.SECRET_KEY;
@@ -17,6 +18,35 @@ app.use('', routes);
 app.get('', (req, res) => {
     res.send('API works');
 });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        const ts = new Date().getTime();
+        const ext = file.originalname.split('.').pop();
+        const name = `${ts}.${ext}`;
+        cb(null, name);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const isValid = file.mimetype.startsWith('image/');
+    cb(null, isValid);
+}
+
+const uploadMiddleware = multer({storage, fileFilter});
+
+app.post('/upload', uploadMiddleware.single('archivo'), (req, res) => {
+    console.log('File:', req.file);
+    if(req.file) {
+        res.send('ok!');
+    } else {
+        res.status(400).send('Invalid Format')
+    }
+    
+})
 
 mongoose.connect(mongoURL).then(client =>{
     app.listen(port,() =>{ 
